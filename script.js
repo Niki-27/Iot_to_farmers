6451015ac05457e7dc78d96f3578cde28fbc3b42
Nikita Choudhary <nikitachoudhary2707@gmail.com>
2023-05-25 22:28:46 +0530

    LogIn Form

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ images.forEach(function(image) {
 
 // Initially show the first image
 showImage(currentIndex);
+const inputs = document.querySelectorAll(".login-box input");
+
+function addClass() {
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("active");
+}
+
+function removeClass() {
+	let parent = this.parentNode.parentNode;
+	if (this.value === "") {
+		parent.classList.remove("active");
+	}
+}
+
+inputs.forEach((input) => {
+	input.addEventListener("focus", addClass);
+	input.addEventListener("blur", removeClass);
+});
